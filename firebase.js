1589268c2c7f5f0b64bf9da8dd6da9abcbae753a
erikseptian09 <121048520+erikseptian09@users.noreply.github.com>
2025-06-06 22:26:17 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getDatabase, ref, set, get, child, remove } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBXnXCSXhy06eqWBPzYmzxCo_LBzl51DO4",
+  authDomain: "wibawamulianetwork.firebaseapp.com",
+  databaseURL: "https://wibawamulianetwork-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wibawamulianetwork",
+  storageBucket: "wibawamulianetwork.firebasestorage.app",
+  messagingSenderId: "605020393823",
+  appId: "1:605020393823:web:e056ed193d9699e64da6ea",
+  measurementId: "G-NNY0Y06HNG"
 };
 
 const app = initializeApp(firebaseConfig);
