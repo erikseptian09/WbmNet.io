782dab38d4f62f28f4665783760c843f0e8ed87e
erikseptian09 <121048520+erikseptian09@users.noreply.github.com>
2025-06-10 14:32:06 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js"; // ✅ TAMBAH INI
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAJ2m4MOTQxw9VgY4IdIO9_E6fiX-9G9VY",
   authDomain: "paymentpelanggan.firebaseapp.com",
   databaseURL: "https://paymentpelanggan-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "paymentpelanggan",
-  storageBucket: "paymentpelanggan.firebasestorage.app",
+  storageBucket: "paymentpelanggan.appspot.com",
   messagingSenderId: "788101237037",
-  appId: "1:788101237037:web:5206ab2fe12a958fa42ba4",
-  measurementId: "G-PJCYE6RDZJ"
+  appId: "1:788101237037:web:5206ab2fe12a958fa42ba4"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const db = getDatabase(app);
 
-export { app, getDatabase, auth }; // ✅ EKSPOR app dan getDatabase
+export { db };
