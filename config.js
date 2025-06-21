4cf251fd0f9fc56eb36c97f0de6e418daf7d87b0
erikseptian09 <121048520+erikseptian09@users.noreply.github.com>
2025-06-21 15:25:02 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJ2m4MOTQxw9VgY4IdIO9_E6fiX-9G9VY",
+  authDomain: "paymentpelanggan.firebaseapp.com",
+  databaseURL: "https://paymentpelanggan-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "paymentpelanggan",
+  storageBucket: "paymentpelanggan.firebasestorage.app",
+  messagingSenderId: "788101237037",
+  appId: "1:788101237037:web:5206ab2fe12a958fa42ba4",
+  measurementId: "G-PJCYE6RDZJ"
+};
+
+firebase.initializeApp(firebaseConfig);
